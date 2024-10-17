0daa5c38d7fb95f05423398ad6415211683ff247
Filipe Silva <filipe.silva@yacoobalabs.com>
2024-10-17 20:24:26 +0100

    feat: added search filter

--- src/screens/HotelsScreen.tsx
+++ src/screens/HotelsScreen.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import HotelsTemplate from '../templates/HotelsTemplate';
 import useHotels from '../hooks/useHotels';
 import {ActivityIndicator} from 'react-native-paper';
+import {Hotel} from '../components/organisms/HotelCard/HotelCard';
+import useSearchFilter from '../hooks/useSearchFilter';
 
 const HotelsScreen = () => {
   const {data, error, isLoading} = useHotels();
-
-  const listData = error ? [] : data;
+  const {listData, searchQuery, filterListData} = useSearchFilter<
+    Hotel,
+    'name'
+  >(data, 'name');
 
   return (
     <>
       {isLoading ? (
         <ActivityIndicator size="large" />
       ) : (
-        <HotelsTemplate data={listData} />
+        <HotelsTemplate
+          searchQuery={searchQuery}
+          data={error ? [] : listData}
+          filterListData={filterListData}
+        />
       )}
     </>
   );
--- src/templates/HotelsTemplate.tsx
+++ src/templates/HotelsTemplate.tsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {FlatList} from 'react-native-gesture-handler';
 import HotelCard, {Hotel} from '../components/organisms/HotelCard/HotelCard';
+import {Searchbar, Text} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 
 type HotelsTemplateProps = {
   data: Array<Hotel>;
+  searchQuery: string;
+  filterListData: (filter: string) => void;
 };
 
-const HotelsTemplate = ({data}: HotelsTemplateProps) => {
+const HotelsTemplate = ({
+  data,
+  filterListData,
+  searchQuery,
+}: HotelsTemplateProps) => {
+  const renderEmptyComponent = useCallback(
+    () => (
+      <View style={styles.emptyContainer}>
+        <Text>No hotels found</Text>
+      </View>
+    ),
+    [],
+  );
+
   return (
-    <FlatList
-      data={data}
-      renderItem={({item}) => {
-        return <HotelCard hotel={item} />;
-      }}
-    />
+    <>
+      <FlatList
+        data={data}
+        ListHeaderComponent={
+          <Searchbar
+            value={searchQuery}
+            placeholder="Search"
+            style={styles.searchbar}
+            onChangeText={filterListData}
+          />
+        }
+        ListEmptyComponent={renderEmptyComponent}
+        renderItem={({item}) => {
+          return <HotelCard hotel={item} />;
+        }}
+      />
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  searchbar: {
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+  emptyContainer: {
+    flex: 1,
+    gap: 10,
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default HotelsTemplate;
